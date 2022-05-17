d9bfd2b80ee6c78a87d1cfb1296951b67b3f356a
begnusupov <71456707+begnusupov@users.noreply.github.com>
2022-05-17 13:45:42 +0300

    Create octal.c
    2 semestr

---
+++ octal.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    long long a = 0;
+    scanf("%lld", &a);
+    printf("%llo", a);
+    return 0;
+}
